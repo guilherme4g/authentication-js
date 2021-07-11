111d16a1e6fa6f98e7f02fc2fd439f8df2b965da
jose guilherme <jgnvs6@gmail.com>
2021-07-11 11:01:18 -0300

    remove babel and use typescript in project

---
+++ src/database/migration/1626010384867-create-table-user-types.ts
@@ -0,0 +1,26 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createTableUserTypes1626010384867 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+        name: 'type_users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+        ]
+      }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('type_users')
+    }
+
+}
