75c026c1b7f4edfab0ab961d8c740b304a6891b5
jose guilherme <jgnvs6@gmail.com>
2021-07-11 21:40:01 -0300

    implement typeUser entity

---
+++ src/modules/users/entities/typeUsers.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+@Entity('type_users')
+export class  TypeUser {
+  @PrimaryGeneratedColumn('uuid')
+  id?: string;
+
+  @Column()
+  name!: string;
+
+  constructor() {
+    if (!this.id) this.id = uuidV4();
+  }
+
+}
